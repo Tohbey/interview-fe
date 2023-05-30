f8e5a590385e18dea90262ade2c3c496ed701b47
tohbey <fafoworatobi25@gmail.com>
2023-05-30 21:18:36 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {ResetPasswordComponent} from "./reset-password/reset-password.component";
+import {SessionExpiredComponent} from "./session-expired/session-expired.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', pathMatch:'full', redirectTo:'login'},
+  {path: '', component:LoginComponent},
+  {path:'reset-password/:token/:email', component: ResetPasswordComponent},
+  {path:'session-expired', component: SessionExpiredComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { SessionExpiredComponent } from './session-expired/session-expired.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    SessionExpiredComponent
   ],
   imports: [
     BrowserModule,
